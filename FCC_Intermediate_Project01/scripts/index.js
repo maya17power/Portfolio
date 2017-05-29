393ce6e5a86134c5acd022c31af49494f9136352
maya17Power <tavo1.ortega1@gmail.com>
2017-05-29 04:35:32 -0700

    fixed issue with json, working on integration with html.

--- FCC_Intermediate_Project01/scripts/index.js
+++ FCC_Intermediate_Project01/scripts/index.js
@@ -3,13 +3,8 @@
 $(document).ready(function(){
   $("#qButton").on("click", function(){
     console.log("Quote button clicked, running command...");
-      $.getJSON("scripts/quotes.json", function(json){
-        $("#quotes").html(JSON.stringify(html));
-        var html = "";
-        json.forEach(function(val){ //test JSON file with this init code.
-          var key = Object.keys(val);
-          return val;
-        });
+      $.getJSON("quotes.json", function(json){
+          console.log(json);
       });
   });
 
