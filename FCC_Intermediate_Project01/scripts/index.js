1e20b0efeade9283ecdb511b38a918a4baf2eb0e
maya17Power <tavo1.ortega1@gmail.com>
2017-05-28 22:14:52 -0700

    Updated intermediate_project01

--- FCC_Intermediate_Project01/scripts/index.js
+++ FCC_Intermediate_Project01/scripts/index.js
@@ -1,7 +1,16 @@
+//jQuery code
+
 $(document).ready(function(){
   $("#qButton").on("click", function(){
     console.log("Quote button clicked, running command...");
-
+      $.getJSON("/scripts/quotes.json", function(json){
+        $("#quotes").html(JSON.stingify(html));
+        var html = "";
+        json.forEach(function(val){ //test JSON file with this init code.
+          var key = Object.keys(val);
+          return val;
+        });
+      });
   });
 
 
