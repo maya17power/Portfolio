d49ce5a3aeff9c9332b7698db41af078c092fa1b
maya17Power <tavo1.ortega1@gmail.com>
2017-05-28 23:06:58 -0700

    update files, debuging json file

--- FCC_Intermediate_Project01/scripts/index.js
+++ FCC_Intermediate_Project01/scripts/index.js
@@ -3,8 +3,8 @@
 $(document).ready(function(){
   $("#qButton").on("click", function(){
     console.log("Quote button clicked, running command...");
-      $.getJSON("/scripts/quotes.json", function(json){
-        $("#quotes").html(JSON.stingify(html));
+      $.getJSON("scripts/quotes.json", function(json){
+        $("#quotes").html(JSON.stringify(html));
         var html = "";
         json.forEach(function(val){ //test JSON file with this init code.
           var key = Object.keys(val);
