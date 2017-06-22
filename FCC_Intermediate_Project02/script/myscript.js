6a38bc51cd249697e2d9d66e080d0eca73c2b9a5
Starhawk <starhawkmbp@Starhawks-MacBook-Pro.local>
2017-06-22 15:09:06 -0700

    updated intermediate project 2 weather app.

--- FCC_Intermediate_Project02/script/myscript.js
+++ FCC_Intermediate_Project02/script/myscript.js
@@ -1,9 +1,31 @@
-$(document).ready(function(){
+var la;
+var long;
+var url = "http://api.openweathermap.org/data/2.5/weather?";
+var unit = "metric";
+var apiKey = "d14a0fc3e2e57bd88fad64796008e203";
 
+$(document).ready(function(){
   if(navigator.geolocation){
-    navigator.geolocation.getCurrentPosition(function(position){
-      $("#data").html("latitude: " + position.coords.latitude + "<br>longitude: " + position.coords.longitude);
-    });
+    navigator.geolocation.getCurrentPosition(position);
   };
-  
+
 });
+
+function position(p){
+  la = p.coords.latitude;
+  long = p.coords.longitude;
+  $("#data").html("latitude: " + la + "<br>longitude: " + long);
+  weather();
+}
+
+function weather(){
+  $.getJSON(url+"lat="+la+"&lon="+long+"&unit="+unit+"&appid="+apiKey,function(w){
+    var skyDetails = w.weather[0].description;
+    console.log(w.weather[0]);
+
+
+
+
+  });
+
+}
