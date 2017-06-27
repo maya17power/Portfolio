6f9460f30cefec3f9a399d022094c98f697acdf4
Starhawk <starhawkmbp@Starhawks-MacBook-Pro.local>
2017-06-26 21:52:40 -0700

    weather app completion

--- FCC_Intermediate_Project02/script/myscript.js
+++ FCC_Intermediate_Project02/script/myscript.js
@@ -1,52 +1,54 @@
-var la;
+var lat;
 var long;
-var rGeo = "https://maps.googleapis.com/maps/api/geocode/json";
-var rGeoApiKey = "AIzaSyCJ6b-AiApzV19TmMhkTWFWXqNBUlfHy48";
-var url = "http://api.openweathermap.org/data/2.5/weather?";
-var unitCel = "metric:Celsius";
-var unitFah = "Imperial:Fahrenheit"
-var apiKey = "d14a0fc3e2e57bd88fad64796008e203";
-var iconUrl = "http://openweathermap.org/img/w/";//using png file extension
-var currentUnit = false;
-$(document).ready(function(){
-  if(navigator.geolocation){
-    navigator.geolocation.getCurrentPosition(position);
-  };
+var rGeo;
+var openWeather;
+var tempUnit = true;
+var https = "https://cors-anywhere.herokuapp.com/";
+$().ready(function(){
+  $("#location, #temp, #tempIcon,#description, #windSpeed").html("<i class='fa fa-spinner fa-spin'></i>");
+  process();
 });
 
-function position(p)
-{
-  la = p.coords.latitude;
-  long = p.coords.longitude;
-  $.getJSON(rGeo + "?latlng="+la+","+long+"&key="+rGeoApiKey,cityState);
+function process(){
+    if(navigator.geolocation){
+    navigator.geolocation.getCurrentPosition(function(val){
+      lat = val.coords.latitude;
+      long = val.coords.longitude;
+      rGeo = "https://maps.googleapis.com/maps/api/geocode/json?latlng="+ lat + "," + long + "&key=AIzaSyCJ6b-AiApzV19TmMhkTWFWXqNBUlfHy48";
+      openWeather = https + "http://api.openweathermap.org/data/2.5/weather?lat=" + lat +"&lon="+ long+"&unit=metric&appid=d14a0fc3e2e57bd88fad64796008e203";;
+
+       cityState();
+    });
+  }
 }
 
-function cityState(location)
-{
-  var city = location.results[0].address_components[2].short_name;
-  var state = location.results[0].address_components[4].short_name;
-  weather(city, state);
-}
-
-function weather(city, state)
-{
-  $.getJSON(url+"lat="+la+"&lon="+long+"&unit="+unitFah+"&appid="+apiKey,function(w)
-  {
-    var temp = w.main.temp;//in kelvin's
-    var cel = Math.round(temp +(-273.15));//&#8451 or &#x2103
-    var fah = Math.round(cel * 1.8 + 32) + '&#8457';//&#8457 or &#x2109
-    var currentTemp = fah ;
-
-    var skyDetails = w.weather[0].description.replace(/(^|\s)[a-z]/g,function(val){return val.toUpperCase();});
-    var icon = w.weather[0].icon+".png";
-    var windSpeed = w.wind.speed+" knt";
-
-    document.getElementById("temp").innerHTML = "<p>"+ currentTemp + "</p> <button type = 'button' id='button'>&#8451</button>";
-    document.getElementById("cityState").innerHTML = "<p>" + city + " , " + state;
-    document.getElementById("weatherIcon").innerHTML = "<img src="+iconUrl+icon+">";
-    document.getElementById("skyDetails").innerHTML = "<p>" + skyDetails + "</p>";
-    document.getElementById("windSpeed").innerHTML = "<p>" + windSpeed + "</p>";
-    document.getElementById("button").addEventListener('click',unitChange, false);
+function cityState(){
+  $.getJSON(rGeo,function(position){
+    var location = position.results[3].address_components[1].short_name + ", " + position.results[3].address_components[3].short_name;
+    $("#location").html("<p>" + location + "</p>");
+  $.getJSON(openWeather,weather);
   });
+}
+
+function weather(w){
+  console.log(w)
+  var kelvin = w.main.temp;
+  var temp;
+
+  if(tempUnit == false){
+    temp = Math.round(kelvin - 273.15) + "&#8451;";
+    $("#unitLabel").html("&#8451;");
+  }else if(tempUnit == true){
+    temp = Math.round(kelvin * (9/5) - 459.67) + "&#8457;";
+    $("#unitLabel").html("&#8457;");
+    console.log(temp);
+           }
+  var description = w.weather[0].description.replace(/(^|\s)[a-z]/g,function(val){return val.toUpperCase();});
+  var windSpeed = w.wind.speed;
+  var icon = "http://openweathermap.org/img/w/" + w.weather[0].icon + ".png";
+  $("#temp").html("<p>Temperature " + temp + "</p>");
+  $("#tempIcon").html("<img src=" + icon + "></img>");
+  $("#description").html("<p>" + description + "</p>");
+  $("#windSpeed").html("<p>Windspeed " + windSpeed + " knt</p>");
 
 }
